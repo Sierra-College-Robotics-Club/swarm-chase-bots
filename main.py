71018d642f0ce7e6a8e688c07b0c014ed54d71b8
Dodo <jduart@mycci.net>
2026-05-26 19:22:47 -0700

    Added smoothing code to minimze false pos WIP

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ frequency = 1000
 led = Pin(25,  Pin.OUT)
 led.on()
 
+lastDirection = 'stop'
+
 #TODO: switch pwm drive to enable pin not the h bridge pins
 
-BOT_NUM = 3
+BOT_NUM = 4
 
 if BOT_NUM == 1:
     midresv = ADC(28)
@@ -41,16 +43,95 @@ elif BOT_NUM == 3:
     led_l = Pin(13,  Pin.OUT)
     
     #h is the motor driver pins (h for H-bridge)
-    enable_a = Pin(2,  Pin.OUT) #enable_a = PWM((2),freq = frequency)
+    enable_a = Pin(2,  Pin.OUT)
     enable_a.on()
     h1 = PWM((3),freq = frequency)
     h2 = PWM((4),freq = frequency)
     h3 = PWM((5),freq = frequency)
     h4 = PWM((6),freq = frequency)
-    enable_b = Pin(7,  Pin.OUT) #enable_b = PWM((7),freq = frequency)
+    enable_b = Pin(7,  Pin.OUT)
     enable_b.on()
     
-
+    def movement(direction, speed):
+    
+        h1.duty_u16(0)
+        h2.duty_u16(0)
+        h3.duty_u16(0)
+        h4.duty_u16(0)
+    
+        if speed > 1:
+            speed = 1
+        
+        duty = ceil(speed * 65535)
+    
+        if direction == "up":
+            h1.duty_u16(duty)
+            h4.duty_u16(duty)
+        elif direction == "down":
+            h2.duty_u16(duty)
+            h3.duty_u16(duty)
+        elif direction == "right":
+            h1.duty_u16(duty)
+            h3.duty_u16(duty)
+        elif direction == "left":
+            h2.duty_u16(duty)
+            h4.duty_u16(duty)
+        else:
+            return False
+        print(direction, speed ,duty, frequency)
+        return True
+    
+elif BOT_NUM == 4:
+    rightresv = ADC(26)
+    midresv = ADC(27)
+    leftresv = ADC(28)
+    
+    led_r = Pin(15,  Pin.OUT)
+    led_m = Pin(14,  Pin.OUT)
+    led_l = Pin(13,  Pin.OUT)
+    
+    #h is the motor driver pins (h for H-bridge)
+    enable_a = PWM((2),freq = frequency)
+    h1 = Pin(3, Pin.OUT)
+    h2 = Pin(4, Pin.OUT)
+    h3 = Pin(5, Pin.OUT)
+    h4 = Pin(6, Pin.OUT)
+    enable_b = PWM((7),freq = frequency)
+    
+    def movement(direction, speed):
+        enable_a.duty_u16(0)
+        enable_b.duty_u16(0)
+        h1.off()
+        h2.off()
+        h3.off()
+        h4.off()
+    
+        if speed > 1:
+            speed = 1
+        
+        duty = ceil(speed * 65535)
+        
+        enable_a.duty_u16(duty)
+        enable_b.duty_u16(duty)
+    
+        if direction == 'up':
+            h1.on()
+            h4.on()
+        elif direction == 'down':
+            h2.on()
+            h3.on()
+        elif direction == 'right':
+            h1.on()
+            h3.on()
+        elif direction == 'left':
+            h2.on()
+            h4.on()
+        else:
+            return False
+        print(direction, speed ,duty, frequency)
+        return True
+        
+'''
 def movement(direction, speed):
     
     h1.duty_u16(0)
@@ -79,11 +160,42 @@ def movement(direction, speed):
         return False
     print(direction, speed ,duty, frequency)
     return True
+'''
+'''
+def movement2(direction, speed):
+    enable_a.duty_u16(0)
+    enable_b.duty_u16(0)
+    
+    if speed > 1:
+        speed = 1
+        
+    duty = ceil(speed * 65535)
+    
+    enable_a.duty_u16(duty)
+    enable_b.duty_u16(duty)
+    
+    if direction == 'up':
+        h1.on()
+        h4.on()
+    elif direction == 'down':
+        h2.on()
+        h3.on()
+    elif direction == 'right':
+        h1.on()
+        h3.on()
+    elif direction == 'left':
+        h2.on()
+        h4.on()
+    else: 
+        return False
+    print(direction, speed ,duty, frequency)
+    return True   
+'''
 
 def irreader(irreceiver):
     minval = 66000
     maxval = 0
-    for i in range (0, 500):
+    for i in range (0, 400):
         reading = irreceiver.read_u16()
         if reading > maxval:
             maxval = reading
@@ -93,6 +205,14 @@ def irreader(irreceiver):
     irstrength = maxval-minval
     #print(irstrength)
     return (irstrength)
+
+def smoothMove(direction, speed):
+    global lastDirection
+    
+    if direction == lastDirection:
+        movement(direction, speed)
+        
+    lastDirection = direction
 
 while True:
     irstrength = irreader(midresv)
@@ -105,24 +225,24 @@ while True:
     led_m.off();
     led_l.off();
     
-    if irstrength2 >= irstrength and irstrength2 >= 1000: #Left
-        movement('left', 0.5)
+    if irstrength2 >= irstrength and irstrength2 >= 2000: #Left
+        smoothMove('left', 0.5)
         led_l.on()
-    elif irstrength3 >= irstrength and irstrength3 >= 1000: #Right
-        movement('right', 0.5)
+    elif irstrength3 >= irstrength and irstrength3 >= 2000: #Right
+        smoothMove('right', 0.5)
         led_r.on()
     elif irstrength > 22000 and not irstrength3 >= irstrength and not irstrength2 >= irstrength:
-        movement("down", 0.75)
+        smoothMove("down", 0.75)
         led_r.on()
         led_l.on()
-    elif irstrength > 1000 and irstrength < 5000:
-        movement("up", 1)
+    elif irstrength > 1000 and irstrength < 5000: 
+        smoothMove("up", 1)
         led_m.on()
     else:
-        movement("stop", 0)
+        smoothMove("stop", 0)
         led_r.on();
         led_m.on();
         led_l.on();
-
+    sleep(0.1)
     
     #movement("up", 0.5*(65535-irstrength)/65535)
