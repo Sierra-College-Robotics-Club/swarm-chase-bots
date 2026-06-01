6ece50e992e597a9efcca13497aede506ce2fc50
Dodo <jduart@mycci.net>
2026-05-31 23:36:10 -0700

    fixed over turning issue in smoothing code

--- main.py
+++ main.py
@@ -9,21 +9,9 @@ led.on()
 
 lastDirection = 'stop'
 
-#TODO: switch pwm drive to enable pin not the h bridge pins
-
-BOT_NUM = 4
+BOT_NUM = 3
 
 if BOT_NUM == 1:
-    midresv = ADC(28)
-    leftresv = ADC(27)
-    rightresv = ADC(26)
-    
-    h1 = PWM((2),freq = frequency)
-    h2 = PWM((3),freq = frequency)
-    h3 = PWM((4),freq = frequency)
-    h4 = PWM((5),freq = frequency)
-
-elif BOT_NUM == 2:
     midresv = ADC(27)
     leftresv = ADC(28)
     rightresv = ADC(26)
@@ -33,7 +21,7 @@ elif BOT_NUM == 2:
     h3 = PWM((4),freq = frequency)
     h4 = PWM((5),freq = frequency)
 
-elif BOT_NUM == 3:
+elif BOT_NUM == 2:
     rightresv = ADC(26)
     midresv = ADC(27)
     leftresv = ADC(28)
@@ -81,7 +69,7 @@ elif BOT_NUM == 3:
         print(direction, speed ,duty, frequency)
         return True
     
-elif BOT_NUM == 4:
+elif BOT_NUM == 3:
     rightresv = ADC(26)
     midresv = ADC(27)
     leftresv = ADC(28)
@@ -130,80 +118,17 @@ elif BOT_NUM == 4:
             return False
         print(direction, speed ,duty, frequency)
         return True
-        
-'''
-def movement(direction, speed):
-    
-    h1.duty_u16(0)
-    h2.duty_u16(0)
-    h3.duty_u16(0)
-    h4.duty_u16(0)
-    
-    if speed > 1:
-        speed = 1
-        
-    duty = ceil(speed * 65535)
-    
-    if direction == "up":
-        h1.duty_u16(duty)
-        h4.duty_u16(duty)
-    elif direction == "down":
-        h2.duty_u16(duty)
-        h3.duty_u16(duty)
-    elif direction == "right":
-        h1.duty_u16(duty)
-        h3.duty_u16(duty)
-    elif direction == "left":
-        h2.duty_u16(duty)
-        h4.duty_u16(duty)
-    else:
-        return False
-    print(direction, speed ,duty, frequency)
-    return True
-'''
-'''
-def movement2(direction, speed):
-    enable_a.duty_u16(0)
-    enable_b.duty_u16(0)
-    
-    if speed > 1:
-        speed = 1
-        
-    duty = ceil(speed * 65535)
-    
-    enable_a.duty_u16(duty)
-    enable_b.duty_u16(duty)
-    
-    if direction == 'up':
-        h1.on()
-        h4.on()
-    elif direction == 'down':
-        h2.on()
-        h3.on()
-    elif direction == 'right':
-        h1.on()
-        h3.on()
-    elif direction == 'left':
-        h2.on()
-        h4.on()
-    else: 
-        return False
-    print(direction, speed ,duty, frequency)
-    return True   
-'''
 
 def irreader(irreceiver):
     minval = 66000
     maxval = 0
-    for i in range (0, 400):
+    for i in range (0, 100):
         reading = irreceiver.read_u16()
         if reading > maxval:
             maxval = reading
         if reading < minval:
             minval = reading
-    #print("Max value mid: ", maxval, "Min value mid: ", minval)
     irstrength = maxval-minval
-    #print(irstrength)
     return (irstrength)
 
 def smoothMove(direction, speed):
@@ -211,32 +136,36 @@ def smoothMove(direction, speed):
     
     if direction == lastDirection:
         movement(direction, speed)
-        
+    elif lastDirection == 'left' and direction == "up" or direction == "down" or direction == "stop":
+        movement('stop', speed)
+    elif lastDirection == 'right' and direction == "up" or direction == "down" or direction == "stop":
+        movement('stop', speed)
+
     lastDirection = direction
 
 while True:
-    irstrength = irreader(midresv)
-    irstrength2 = irreader(leftresv)
-    irstrength3 = irreader(rightresv)
+    irstrengthMid = irreader(midresv)
+    irstrengthLeft = irreader(leftresv)
+    irstrengthRight = irreader(rightresv)
     
-    print(irstrength, irstrength2, irstrength3)
+    print(irstrengthLeft, irstrengthMid, irstrengthRight)
     
     led_r.off();
     led_m.off();
     led_l.off();
     
-    if irstrength2 >= irstrength and irstrength2 >= 2000: #Left
+    if irstrengthLeft >= irstrengthMid and irstrengthLeft >= 2000: #Left
         smoothMove('left', 0.5)
         led_l.on()
-    elif irstrength3 >= irstrength and irstrength3 >= 2000: #Right
+    elif irstrengthRight >= irstrengthMid and irstrengthRight >= 2000: #Right
         smoothMove('right', 0.5)
         led_r.on()
-    elif irstrength > 22000 and not irstrength3 >= irstrength and not irstrength2 >= irstrength:
+    elif irstrengthMid > 22000 and not irstrengthRight >= irstrengthMid and not irstrengthLeft >= irstrengthMid: #Backup
         smoothMove("down", 0.75)
         led_r.on()
         led_l.on()
-    elif irstrength > 1000 and irstrength < 5000: 
-        smoothMove("up", 1)
+    elif irstrengthMid > 1000 and irstrengthMid < 5000: #Forward
+        smoothMove("up", 0.9)
         led_m.on()
     else:
         smoothMove("stop", 0)
@@ -244,5 +173,3 @@ while True:
         led_m.on();
         led_l.on();
     sleep(0.1)
-    
-    #movement("up", 0.5*(65535-irstrength)/65535)
