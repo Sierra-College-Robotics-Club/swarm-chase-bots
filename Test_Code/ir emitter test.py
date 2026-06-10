32e707d2873436a4a3adeb2c34f99dfdcba8ca29
Dodo <jduart@mycci.net>
2026-06-10 14:58:06 -0700

    Added transmiter button toggle to turn on IR

--- Test_Code/ir emitter test.py
+++ Test_Code/ir emitter test.py
@@ -2,13 +2,51 @@ from machine import Pin,PWM,ADC
 from time import sleep
 from math import ceil
 
-led = Pin(25,  Pin.OUT)
-led.on()
-p16 = Pin(20,  Pin.OUT)
+whatcode = "new"
 
-duty = ceil(0.5 * 65535)
+if whatcode == "old":
+    #OG code
+    led = Pin(25,  Pin.OUT)
+    led.on()
+    p16 = Pin(20,  Pin.OUT)
 
-pwm16 = PWM(p16)
+    duty = ceil(0.5 * 65535)
 
-pwm16.freq(1000)
-pwm16.duty_u16(duty)
+    pwm16 = PWM(p16)
+
+    pwm16.freq(1000)
+    pwm16.duty_u16(duty)
+
+if whatcode == "new":
+    led = Pin(25,  Pin.OUT)
+    led.on()
+
+    pin12 = Pin(12, Pin.IN)
+    pin16 = Pin(20,  Pin.OUT)
+
+    pwm16 = PWM(pin16)
+    duty = ceil(0.5 * 65535)
+    value = 0
+
+    def pulse_on(pin):
+        global value
+        if pin.value() == 1:
+            print('On')
+            value = 1
+        else:
+            print('Off')
+            value = 0
+    
+    pin12.irq(trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING, handler=pulse_on)
+
+    while True:
+        if value == 1:
+            pwm16 = PWM(pin16)
+            pwm16.freq(1000)
+            pwm16.duty_u16(duty)
+            #print('1')
+            sleep(0.1)
+        elif value == 0:
+            pwm16.deinit()
+            #print('0')
+            sleep(0.1)
